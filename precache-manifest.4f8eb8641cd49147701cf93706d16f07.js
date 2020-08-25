55d97697a8907acda76b0034a5bd3c7083f181e7
Bharath Kumar <pbkbharath1707@gmail.com>
2020-08-25 14:59:55 +0530

    Updates

---
+++ precache-manifest.4f8eb8641cd49147701cf93706d16f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ba65676abfb35ab0e14abff1047733",
+    "url": "/Tinder-Bharath/index.html"
+  },
+  {
+    "revision": "812cc03c9a5984da5c61",
+    "url": "/Tinder-Bharath/static/css/main.09e74193.chunk.css"
+  },
+  {
+    "revision": "e7e8b2be18cac115d35e",
+    "url": "/Tinder-Bharath/static/js/2.80a6be50.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Tinder-Bharath/static/js/2.80a6be50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812cc03c9a5984da5c61",
+    "url": "/Tinder-Bharath/static/js/main.75ffed04.chunk.js"
+  },
+  {
+    "revision": "bc76dc68a5880fac1692",
+    "url": "/Tinder-Bharath/static/js/runtime-main.580d61d8.js"
+  }
+]);
